b811d619d0c043e3e8d9de196f500487fc799b9f
gregoriB <brandon.gregori@gmail.com>
2024-01-16 17:02:25 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'super_balloon_popper',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
