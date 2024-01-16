0786bec621a69dc47cb64b43d465366717e00bc9
gregoriB <brandon.gregori@gmail.com>
2024-01-16 18:40:34 -0500

    Rename app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'super_balloon_popper',
+  appName: 'mobile_app',
   webDir: 'www',
   server: {
     androidScheme: 'https',
