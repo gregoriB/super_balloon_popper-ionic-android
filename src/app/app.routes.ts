3afc5fe20ec606e45b2581e77c64468b64d6c1d0
gregoriB <brandon.gregori@gmail.com>
2024-01-16 19:04:45 -0500

    Remove unused components

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Route } from '@angular/router';
-
-export const routes: Route[] = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
-];
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,13 @@
-// import { enableProdMode, importProvidersFrom } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-//
-// import { AppModule } from './app/app.module';
-// import { environment } from './environments/environment';
-//
-// if (environment.production) {
-//   enableProdMode();
-// }
-//
-// platformBrowserDynamic().bootstrapModule(AppModule)
-//   .catch(err => console.log(err));
-//
+import { importProvidersFrom } from '@angular/core';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
-    provideRouter(routes),
   ],
 });
