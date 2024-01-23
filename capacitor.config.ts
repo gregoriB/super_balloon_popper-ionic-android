44d5f1d3bf3f9454de438198c868c0dd986fcc44
gregoriB <brandon.gregori@gmail.com>
2024-01-22 22:21:26 -0500

    Update icon and splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'io.ionic.starter',
-    appName: 'mobile_app',
+    appName: 'Balloon Popper!',
     webDir: 'www',
     server: {
         androidScheme: 'https',
