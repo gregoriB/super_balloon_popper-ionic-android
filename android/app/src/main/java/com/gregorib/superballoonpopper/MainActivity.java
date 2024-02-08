e602aaa41e6cf9170ee78ab7824b2cbabdc45925
gregoriB <brandon.gregori@gmail.com>
2024-02-07 20:06:04 -0500

    Update package name and app id

--- android/app/src/main/java/com/gregorib/superballoonpopper/MainActivity.java
+++ android/app/src/main/java/com/gregorib/superballoonpopper/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.gregorib.superballoonpopper;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'io.ionic.starter',
+    appId: 'com.gregorib.superballoonpopper',
     appName: 'Super Balloon Popper!',
     webDir: 'www',
     server: {
